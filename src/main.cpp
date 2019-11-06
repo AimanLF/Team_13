d1b2532379be8b704514c2bb905297cada7ff409
Lavallard Fadlane Aïman Guilain <lavallar@INTRANET.EPFL.CH@SVUbuUniv21.intranet.epfl.ch>
2019-11-06 12:58:42 +0100

    Completed TCLAP v1

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,107 @@
 #include <tclap/CmdLine.h>
 #include <string>
 
-using namespace TCLAP;
-using namespace std;
 
 int main(int argc, char ** argv) {
   	
-  CmdLine cmd("commande");
+  TCLAP::CmdLine cmd("commande");
   
+  //Saisie par l'utilisateur
+  TCLAP::ValueArg <std::string> file_name("F", "file name", "File with parameters", false, "", "string");
+  cmd.add(file_name);
+
   //Taille de la population
-  ValueArg <long unsigned int> population_size("N", "size", "number of individuals", true, 100, "long unsigned int");
+  TCLAP::ValueArg <size_t> population_size("N", "size", "number of individuals", true, 100, "size_t");
   cmd.add(population_size);
 
   //Durée (en nombre de générations) de la simulation 
-  ValueArg <long unsigned int> duration("T", "time", "number of generations", true, 10, "long unsigned int");
+  TCLAP::ValueArg <size_t> duration("T", "time", "number of generations", true, 10, "size_t");
   cmd.add(duration);
 
   //Nombre d’allèles dans la population
-  ValueArg <unsigned int> alleles_number("A", "alleles number", "number of alleles in the population", true, 2, "unsigned int");
+  TCLAP::ValueArg <size_t> alleles_number("A", "alleles number", "number of alleles in the population", true, 2, "size_t");
   cmd.add(alleles_number);
   
   //Fréquences initiales des allèles 
-  MultiArg<double> freq("f", "frequences", "initial alleles' frequencies (as fraction of the population)", .8, true,"int" );
+  TCLAP::MultiArg<double> freq("f", "frequences", "frequencies (as fraction of the population)", true, "double" );
   cmd.add(freq);
 
   //la simulation sera répétée R fois
-  ValueArg <unsigned int> repetitions("R", "number of repetions", "simulation will be repeted R times", true, 2, "unsigned int");
+  TCLAP::ValueArg <size_t> repetitions("R", "number of repetions", "simulation will be repeted R times", true, 2, "size_t");
   cmd.add(repetitions);
   
   cmd.parse(argc, argv);
   
+  if(file_name.getValue() != "" ) {
+	//on lit le fichier
+	  	//if la fonction qui lit le ficher renvoit un message d'erreur on utilise les valeurs par défauts
+	 } 
+	  
   if (population_size.getValue() < 1) {
-    cerr << "Population size must be strictly positive" << endl;
+    std::cerr << "Population size must be strictly positive" << std::endl;
     return 1;
   }
   
   if (duration.getValue() < 1) {
-    cerr << "Simulation duration must be strictly positive" << endl;
+    std::cerr << "Simulation duration must be strictly positive" << std::endl;
     return 1;
   }
   
   if (alleles_number.getValue() < 1) {
-    cerr << "Number of alleles must be strictly positive" << endl;
+    std::cerr << "Number of alleles must be strictly positive" << std::endl;
     return 1;
   }
   
-  for(auto f : freq) {
-	  if (f.getValue() < 0 or f.getValue() > 1)  {
-		  cerr << "Frequencies must be between 0 and 1 included" << endl;
+  
+  double total_freq(0);
+  for(auto f : freq.getValue()) {
+		total_freq += f;
+	  if (total_freq != 1) {
+		   std::cerr << "Sum of frequencies must be equal to 1" << std::endl;
+	  
+	  if (f < 0 or f > 1)  {
+		  std::cerr << "Frequencies must be between 0 and 1 included" << std::endl;
+	   
+		  
 		  return 1;
+		}
+	  }
   
    if (repetitions.getValue() < 1) {
-    cerr << "Number of repetitions must be strictly positive" << endl;
+    std::cerr << "Number of repetitions must be strictly positive" << std::endl;
     return 1;
   }
   
+}
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
