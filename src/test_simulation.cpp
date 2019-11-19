1e068c1bccc884c5b63b00f89be0c662f65fed35
Dormann Alexia Amandine <dormann@INTRANET.EPFL.CH@SVUbuUniv17.intranet.epfl.ch>
2019-11-19 20:38:51 +0100

    Test compile

--- src/test_simulation.cpp
+++ src/test_simulation.cpp
@@ -13,7 +13,7 @@ size_t nbPop(100);
 
 //Test if after 1, 2 and 3 generation(s) same average frequencies on 100 populations with same frequencies
  for (size_t i(0); i < 3; i++){
-    Simulation simulation(i,nbPop,300,5,std::vector<double> = {freq1, freq2, freq3, freq4, freq5}, std::vector<std::string> (5,'')); 
+    Simulation simulation(i,nbPop,300,5,std::vector<double> {freq1, freq2, freq3, freq4, freq5}, std::vector<std::string> (5,"-")); 
     simulation.run();
     
     double f1 = 0, f2 = 0, f3 = 0, f4 = 0, f5 = 0;
@@ -34,25 +34,31 @@ size_t nbPop(100);
 }
 
 TEST(simulationTest,fixation_time){
-double freqx (0.6);
-double freqy (0.4);
-Simulation simulation1(300,1,100,2,std::vector<double> = {freqx, freqy}, std::vector<std::string> (2,''));
-simulation1.run();
-bool cond1(1.0 == simulation1.getFreqPop(0) or 1.0 == simulation1.getFreqPop(1));		//float ?
+	//Test si une allèle se fixe avec 2 allèles
+	double freqx (0.6);
+	double freqy (0.4);
+	Simulation simulation1(300,1,100,2,std::vector<double> {freqx, freqy}, std::vector<std::string> (2,"-"));
+	simulation1.run();
 
-double freq1 (0.3);
-double freq2 (0.4);
-double freq3 (0.1);
-double freq4 (0.05);
-double freq5 (0.15);
-Simulation simulation2(300,1,100,2,std::vector<double> = {freq1, freq2, freq3, freq4, freq5}, std::vector<std::string> (5,''));
-simulation2.run();
-bool cond2(1.0 == simulation2.getFreqPop(0) or 1.0 == simulation2.getFreqPop(1) or 1.0 == simulation2.getFreqPop(2)
-			or 1.0 == simulation2.getFreqPop(3) or 1.0 == simulation2.getFreqPop(4));		//float ?
+	std::vector <double> f1 (simulation1.getFreqPop(0));
+	bool cond1(1.0 == f1[0] or 1.0 == f1[1]);		//float ?
+
+	//Test si une allèle se fixe avec 5 allèles
+	double freq1 (0.3);
+	double freq2 (0.4);
+	double freq3 (0.1);
+	double freq4 (0.05);
+	double freq5 (0.15);
+	Simulation simulation2(300,1,100,2,std::vector<double> {freq1, freq2, freq3, freq4, freq5}, std::vector<std::string> (5,"-"));
+	simulation2.run();
+	
+	std::vector <double> f2 (simulation2.getFreqPop(0));
+	bool cond2(1.0 == f2[0] or 1.0 == f2[1] or 1.0 == f2[2]
+				or 1.0 == f2[3] or 1.0 == f2[4]);		//float ?
 
 
-EXPECT_TRUE(cond1);
-EXPECT_TRUE(cond2);
+	EXPECT_TRUE(cond1);
+	EXPECT_TRUE(cond2);
 
 }
 
