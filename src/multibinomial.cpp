38731fbae9ebf72a53a31c32efa937c4d0ce8dea
Chillier Noémie <chillier@INTRANET.EPFL.CH@SVUbuUniv17.intranet.epfl.ch>
2019-11-23 17:06:42 +0100

    correction pour compilation

--- src/multibinomial.cpp
+++ src/multibinomial.cpp
@@ -1,4 +1,3 @@
-#include <random>
 #include <iostream>
 #include <vector>
 #include <multibinomial.h>
--- src/test_multibinomial.cpp
+++ src/test_multibinomial.cpp
@@ -1,34 +1,46 @@
 #include <gtest/gtest.h>
-#include "multibinomial.cpp"
+#include "multibinomial.h"
 #include <random>
 #include <vector>
 
-std::mt19937 rng;
+/*std::mt19937 rng;
 std::random_device rd;
 rng = std::mt19937(rd());
+* 
+std::random_device rd;
+std::mt19937 rng(rd());
+*/
+
+//variable externe, comment faire mieux ? 
+extern std::random_device rd;
+extern std::mt19937 rng = std::mt19937(rd());
 
 TEST(MultibinomialTest,somme1){
 //Test que la somme des allèles est toujours équales à 1 après 10 appels de multibinomial
   std::vector<double> test = {0.3,0.2,0.4,0.1};
   
-  for(size_t i(0); i < 10; i++)
-    test = multibinomial(4,test,rng);
+  for(size_t i(0); i < 10; i++) {
+    test = multibinomial(100,test,rng);
   
-  double somme (0);
-  for(auto f : test)
-    somme += f;
-  
-  EXPECT_NEAR(1, somme, 1e-4);
+	double somme (0.0);
+	for(auto f : test) {
+		somme += f;
+	}
+	
+	EXPECT_NEAR(1, somme, 1e-4);
+	}
+
 }
 
 TEST(MultibinomialTest, fixation){
  //Test que une allèles étant = 0 ne peut pas réapparaître
  std::vector<double> test = {0.3,0.2,0.5,0.0};
  
- for(size_t i(0); i < 10; i++)
-    test = multibinomial(4,test,rng);
+ for(size_t i(0); i < 10; i++) {
+    test = multibinomial(100,test,rng);
     
-    EXCEPT(0, test[3]);
+    EXPECT_TRUE(test[3] == 0.0);
+    }
   
 }
 
