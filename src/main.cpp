973b0af1e9dd80468898ca1b5b81b148e9bd008d
Constantin Loris <loconsta@INTRANET.EPFL.CH@SVUbuUniv19.intranet.epfl.ch>
2019-11-27 21:42:58 +0100

    main modifié selon migration, avec gestion d erreur

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <tclap/CmdLine.h>
 #include "read_fasta.h"
-#include "simulation.h"
-//#include "migration.h"
+#include "migration.h"
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -16,65 +15,49 @@ int main(int argc, char ** argv) {
 	  TCLAP::CmdLine cmd("commande");
   
 	  //Saisie par l'utilisateur
-	  TCLAP::ValueArg <std::string> file_name("f", "file_name", "File with parameters", false, "", "string");
+	  TCLAP::ValueArg <std::string> file_name("F", "File_name", "Fasta file containing sequences.", false, "", "string");
 	  cmd.add(file_name);
 	  
 	  //Affichage terminal et/ou fichier
-	  TCLAP::ValueArg <bool> terminal("e", "terminal", "Print results in terminal", false, "", "bool");
+	  TCLAP::ValueArg <bool> terminal("T", "Terminal", "Print results in terminal.", false, false, "bool");
 	  cmd.add(terminal);
-	  TCLAP::ValueArg <bool> print_file("i", "print_file", "Print results in a file", false, "", "bool");
+	  TCLAP::ValueArg <bool> print_file("p", "print_file", "Print results in a file.", false, false, "bool");
 	  cmd.add(print_file);
 
 	  //Taille de la population
-	  TCLAP::ValueArg <int> population_size("n", "size", "number of individuals", false, 100, "int");
+	  TCLAP::ValueArg <int> population_size("n", "size", "Number of individuals.", false, 100, "int");
 	  cmd.add(population_size);
 
 	  //Durée en nombre de générations 
-	  TCLAP::ValueArg <int> duration("t", "time", "number of generations", false, 10, "int");
-	  cmd.add(duration);
+	  TCLAP::ValueArg <int> generation("g", "generations", "Number of generations.", false, 10, "int");
+	  cmd.add(generation);
 
 	  //Fréquences 
-	  TCLAP::MultiArg<double> freq("p", "frequences", "frequencies (as fraction of the population)", false, "double" );
+	  TCLAP::MultiArg<double> freq("f", "frequences", "Frequencies (as fraction of the population).", false, "double" );
 	  cmd.add(freq);
 
 	  //Répétitions de la simulation
-	  TCLAP::ValueArg <int> repetitions("r", "number_repetions", "simulation will be repeated R times", false, 3, "int");
+	  TCLAP::ValueArg <int> repetitions("r", "number_repetions", "Simulation will be repeated R times.", false, 3, "int");
 	  cmd.add(repetitions);
 	  
 	  //marqueurs	
-	  TCLAP::MultiArg<size_t> markers("m", "markers", "alleles' positions on the sequence", false, "size_t" );
+	  TCLAP::MultiArg <size_t> markers("m", "markers", "Alleles' positions on the sequence.", false, "size_t" );
 	  cmd.add(markers);
 	  
-	  //Extension:
-	  //Remplissage de la matrice 
-	  TCLAP::ValueArg <std::string> star("s", "star", "Star migration pattern", false, "","string");
-      TCLAP::ValueArg <std::string> ring("g", "ring", "Ring migration pattern", false, "", "string");
-	  TCLAP::ValueArg <std::string> complete("c", "complete", "Complete migration pattern", false, "", "string");
-      std::vector<TCLAP::Arg*>  xorlist;
-      xorlist.push_back(&star);
-      xorlist.push_back(&ring);
-      xorlist.push_back(&complete);
-      cmd.xorAdd(xorlist);
-
-	  
-	  //Ratio de population à migrer
-	  TCLAP::ValueArg <double> migration_ratio("a", "migration_ratio", "Percentage of the population to move", false, 0, "double");
-	  cmd.add(migration_ratio);
+	  //Type et ratio de migration
+	  TCLAP::ValueArg <std::string> migration_type("M", "Migration_type", "Choose a migration type (star/ring/complete).", false, "","string");
+      cmd.add(migration_type);
+      TCLAP::ValueArg <double> ratio("R", "Ratio", "Choose a migration ratio.", false, 0, "double");
+	  cmd.add(ratio);
 	  
 	  cmd.parse(argc, argv);		//trier les exeptions en fonctions du fasta/terminal
 	   
 	   
 	  //Gestion d'erreurs 	
-	  if (!terminal.isSet() and !print_file.isSet()) throw TCLAP::ArgException("At least one output (terminal/file) has to specified"); 
-	  if (duration.getValue() < 1) throw TCLAP::ArgException("Simulation duration must be strictly positive");
-	  if (repetitions.getValue() < 1) throw TCLAP::ArgException("Number of repetitions must be strictly positive");
-	  if ( (repetitions.getValue() < 0) or (repetitions.getValue() > 1) ) throw TCLAP::ArgException("Ratio has to be between 0 and 1");
-	  
-	  std::string migration_type;
-	  if (star.isSet()) migration_type = star.getValue();
-      else if(ring.isSet()) migration_type = ring.getValue();
-      else migration_type = "";
-
+	  if (!terminal.isSet() and !print_file.isSet()) throw TCLAP::ArgException("At least one output (terminal/file) has to specified."); 
+	  if (generation.getValue() < 1) throw TCLAP::ArgException("Simulation duration must be strictly positive.");
+	  if (repetitions.getValue() < 1) throw TCLAP::ArgException("Number of repetitions must be strictly positive.");
+	  if ( (repetitions.getValue() < 0) or (repetitions.getValue() > 1) ) throw TCLAP::ArgException("Ratio has to be between 0 and 1.");
 	  
 	  std::vector<std::string> _genetic_code;
 	  std::vector<double> _freqs(freq.getValue());
@@ -83,10 +66,9 @@ int main(int argc, char ** argv) {
 	  size_t _alleles_number((freq.getValue()).size());
 	  std::string _file_name(file_name.getValue());
 	  
-		if(file_name.isSet()) {
+		if(file_name.isSet()) {													//option fasta
 			
 			try {
-				//for (auto val : markers.getValue()) if (val < 1) throw TCLAP::ArgException("Markers must be strictly positive"); //les size_t négatifs sont convertis en positifs donc pas de sens
 				read_fasta(_freqs, _genetic_code, _markers, _population_size, _alleles_number, _file_name);
 			} catch(std::invalid_argument &e) {
 				std::cerr << e.what() << std::endl;
@@ -98,19 +80,23 @@ int main(int argc, char ** argv) {
 			double total_freq(0);
 			for(auto f : freq.getValue()) {
 				total_freq += f;
-				if (f < 0 or f > 1) throw TCLAP::ArgException("Frequencies must be between 0 and 1 included");
+				if (f < 0 or f > 1) throw TCLAP::ArgException("Frequencies must be between 0 and 1 included.");
 				}
-			if (1-total_freq > 0.001 or 1-total_freq < -0.001) throw TCLAP::ArgException("Sum of frequencies must be equal to 1");
-			if (population_size.getValue() < 1) throw TCLAP::ArgException("Population size must be strictly positive");
+			if (1-total_freq > 0.001 or 1-total_freq < -0.001) throw TCLAP::ArgException("Sum of frequencies must be equal to 1.");
+			if (population_size.getValue() < 1) throw TCLAP::ArgException("Population size must be strictly positive.");
 			
 			for (size_t i(0) ; i < (freq.getValue()).size() ; ++i) _genetic_code.push_back(std::to_string(i+1));		
 		}
 		
-		if(migration_type != "") {					
-			//Migration migration(duration.getValue(), repetitions.getValue(), _population_size, _alleles_number, terminal.getValue(), print_file.getValue(), _freqs, _genetic_code, migration_type,migration_ratio.getValue());
-			//migration.run();
+		if(migration_type.isSet() and ratio.isSet()) {							//option migration		
+			std::string t = migration_type.getValue();
+			if (t != "star" and t != "ring" and t != "complete") throw TCLAP::ArgException("Migration type invalid.");
+			if (0 <= ratio.getValue() or ratio.getValue() >= 1) throw TCLAP::ArgException("Migration ratio must be stricly BETWEEN 0 and 1.");
+			
+			Migration migration(generation.getValue(), repetitions.getValue(), _population_size, _alleles_number, terminal.getValue(), print_file.getValue(), _freqs, _genetic_code, migration_type.getValue() , ratio.getValue());
+			migration.run();
 		} else {
-			Simulation simulation(duration.getValue(), repetitions.getValue(), _population_size, _alleles_number, terminal.getValue(), print_file.getValue(), _freqs, _genetic_code);
+			Simulation simulation(generation.getValue(), repetitions.getValue(), _population_size, _alleles_number, terminal.getValue(), print_file.getValue(), _freqs, _genetic_code);
 			simulation.run();
 		}
 		
