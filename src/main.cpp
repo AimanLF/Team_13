fe138b0e51d3868ffd82ad1babc0dfdf4147329f
AlbrechtAlice <56924814+AlbrechtAlice@users.noreply.github.com>
2019-11-29 15:49:40 +0100

    Ajout erreur si pas de markers pour lire le file

--- src/main.cpp
+++ src/main.cpp
@@ -69,6 +69,7 @@ int main(int argc, char ** argv) {
 		if(file_name.isSet()) {													//option fasta
 			
 			try {
+				if (!markers.isSet()) throw ("Expect markers to read the file");
 				read_fasta(_freqs, _genetic_code, _markers, _population_size, _alleles_number, _file_name);
 			} catch(std::invalid_argument &e) {
 				std::cerr << e.what() << std::endl;
