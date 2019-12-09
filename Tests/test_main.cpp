4cc346fec47de9320ce13773e77770454c0385b3
AlbrechtAlice <56924814+AlbrechtAlice@users.noreply.github.com>
2019-12-09 13:37:38 +0100

    Delete test_main.cpp

--- Tests/test_main.cpp
+++
@@ -1,7 +0,0 @@
-#include "test_simulation_migration.cpp"
-#include "test_multibinomial.cpp"
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
