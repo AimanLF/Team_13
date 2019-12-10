a4002d0c2897fecffe558ec6d7dbe3afa88288e2
Chillier Noémie <chillier@INTRANET.EPFL.CH@SVUbuUniv16.intranet.epfl.ch>
2019-12-10 19:35:06 +0100

    add test sort

--- test/test_readfasta.cpp
+++ test/test_readfasta.cpp
@@ -2,7 +2,7 @@
 #include "../src/read_fasta.h"
 #include <vector>
 
-TEST(readFastaTest, readNormalFasta){
+TEST(ReadFastaTest, readNormalFasta){
 	std::vector<double> freq;
 	std::vector<std::string> alle;
 	int nbInd(0);
@@ -18,7 +18,7 @@ TEST(readFastaTest, readNormalFasta){
 	EXPECT_EQ("TCC", alle[0]);
 	EXPECT_EQ("TGC", alle[1]);
 }
-TEST(readFastaTest, readBadlyWrittenFasta){
+TEST(ReadFastaTest, readBadlyWrittenFasta){
 	std::vector<double> freq;
 	std::vector<std::string> alle;
 	int nbInd(0);
@@ -33,4 +33,20 @@ TEST(readFastaTest, readBadlyWrittenFasta){
 	EXPECT_NEAR(0.4286,freq[0],1e-4);
 	EXPECT_EQ("TCC", alle[0]);
 	EXPECT_EQ("TGC", alle[1]);
+}
+
+TEST(ReadFastaTest, sort)
+{
+	std::vector<std::string> mix_codon {"AAG", "ATG", "AAC", "TCG", "CGC", "GAT"};
+	std::vector<double> mix_frequences {0.10, 0.20, 0.05, 0.35, 0.08, 0.22};
+	
+	sort(mix_frequences, mix_codon);
+	
+	std::vector<std::string> sort_codon {"AAC", "AAG", "ATG", "CGC", "GAT", "TCG"};
+	std::vector<double> sort_frequences { 0.05, 0.10, 0.20, 0.08, 0.22, 0.35};
+	
+	for(size_t i(0); i < mix_codon.size(); ++i) {
+		EXPECT_TRUE(mix_codon[i] == sort_codon[i]);
+		EXPECT_TRUE(mix_frequences[i] == sort_frequences[i]);
+	}
 }
