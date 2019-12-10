aed9af6bbb2d2bce0449f20e82f5ddf6ab76a53f
Dormann Alexia Amandine <dormann@INTRANET.EPFL.CH@SVUbuUniv16.intranet.epfl.ch>
2019-12-10 12:50:30 +0100

    Test fasta okay

--- test/test_readfasta.cpp
+++ test/test_readfasta.cpp
@@ -18,3 +18,19 @@ TEST(readFastaTest, readNormalFasta){
 	EXPECT_EQ("TCC", alle[0]);
 	EXPECT_EQ("TGC", alle[1]);
 }
+TEST(readFastaTest, readBadlyWrittenFasta){
+	std::vector<double> freq;
+	std::vector<std::string> alle;
+	int nbInd(0);
+	size_t nbAll(0);
+	std::string fichier("../data_base/test_bad.fa");
+	
+	read_fasta(freq, alle, {1,2,3}, nbInd, nbAll, fichier);
+	
+	EXPECT_EQ(21,nbInd);
+	EXPECT_EQ(2,nbAll);
+	EXPECT_NEAR(0.5714,freq[1],1e-4);
+	EXPECT_NEAR(0.4286,freq[0],1e-4);
+	EXPECT_EQ("TCC", alle[0]);
+	EXPECT_EQ("TGC", alle[1]);
+}
