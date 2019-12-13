5ecce422aa473018e85f591436ed7ff92389b1c7
Aïman LF <48866499+AimanLF@users.noreply.github.com>
2019-12-13 15:11:17 +0100

    Update test_multibinomial.cpp

--- test/test_multibinomial.cpp
+++ test/test_multibinomial.cpp
@@ -6,7 +6,7 @@
   TEST(MultibinomialTest, fixation): Test qu'un allèle à 0 ne peut pas réapparaître
  */
  
-TEST(MultibinomialTest,somme1){
+TEST(MultibinomialTest, somme1){
 	std::vector<double> test = {0.3,0.2,0.4,0.1};
 	for(size_t i(0); i < 10; i++) {
 		test = multibinomial(100,test);
